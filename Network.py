3b022ba75bf7a4fa94c5f3f064894192a0754836
Sergey Savelyev <sergeysav.nn@gmail.com>
2019-01-25 15:08:53 -0500

    removes channels-first

--- Network.py
+++ Network.py
@@ -16,12 +16,12 @@ REG_PARAM = 0.01
 
 def residual_block(previous):
     # A convolution of 256 filters of kernel size 3 × 3 with stride 1
-    conv1 = Conv2D(256, kernel_size=3, strides=1, activation='relu', kernel_regularizer=l2(REG_PARAM), padding="same", data_format="channels_first")(previous)
-    norm1 = BatchNormalization(axis=1)(conv1)  # Batch normalisation
+    conv1 = Conv2D(256, kernel_size=3, strides=1, activation='relu', kernel_regularizer=l2(REG_PARAM), padding="same")(previous)
+    norm1 = BatchNormalization()(conv1)  # Batch normalisation
     rnln1 = ReLU()(norm1)  # A rectifier non-linearity
     # A convolution of 256 filters of kernel size 3 × 3 with stride 1
-    conv2 = Conv2D(256, kernel_size=3, strides=1, activation='relu', kernel_regularizer=l2(REG_PARAM), padding="same", data_format="channels_first")(rnln1)
-    norm2 = BatchNormalization(axis=1)(conv2)  # Batch normalisation
+    conv2 = Conv2D(256, kernel_size=3, strides=1, activation='relu', kernel_regularizer=l2(REG_PARAM), padding="same")(rnln1)
+    norm2 = BatchNormalization()(conv2)  # Batch normalisation
     skip = add([norm2, previous])  # A skip connection that adds the input to the block
     rnln2 = ReLU()(skip)  # A rectifier non-linearity
     return rnln2
@@ -29,11 +29,11 @@ def residual_block(previous):
 
 class Network:
     def __init__(self):
-        self.input = Input(shape=(M*T + L, N, N))
+        self.input = Input(shape=(N, N, M*T + L))
 
         # A convolution of 256 filters of kernel size 3 × 3 with stride 1
-        conv = Conv2D(256, kernel_size=3, strides=1, activation='relu', kernel_regularizer=l2(REG_PARAM), padding="same", input_shape=(M*T + L, N, N), data_format="channels_first")(self.input)
-        norm = BatchNormalization(axis=1)(conv)  # Batch normalisation
+        conv = Conv2D(256, kernel_size=3, strides=1, activation='relu', kernel_regularizer=l2(REG_PARAM), padding="same", input_shape=(N, N, M*T + L))(self.input)
+        norm = BatchNormalization()(conv)  # Batch normalisation
         rnln = ReLU()(norm)  # A rectifier non-linearity
 
         tower = rnln
@@ -41,21 +41,21 @@ class Network:
             tower = residual_block(tower)
 
         # A convolution of 2 filters of kernel size 1 × 1 with stride 1
-        policy_conv = Conv2D(2, kernel_size=1, strides=1, activation='relu', kernel_regularizer=l2(REG_PARAM), padding="same", data_format="channels_first")(tower)
-        policy_norm = BatchNormalization(axis=1)(policy_conv)  # Batch normalisation
+        policy_conv = Conv2D(2, kernel_size=1, strides=1, activation='relu', kernel_regularizer=l2(REG_PARAM), padding="same")(tower)
+        policy_norm = BatchNormalization()(policy_conv)  # Batch normalisation
         policy_rnln = ReLU()(policy_norm)  # A rectifier non-linearity
         # Make sure the output size is correct
-        policy_flat = Flatten(data_format="channels_first")(policy_rnln)
+        policy_flat = Flatten()(policy_rnln)
         # A fully connected linear layer that outputs a vector of size 192 + 1 = 362 corresponding to logit
         # probabilities for all intersections and the pass move
         policy_dout = Dense(73 * 8 * 8, kernel_regularizer=l2(REG_PARAM))(policy_flat)
 
         # A convolution of 1 filter of kernel size 1 × 1 with stride 1
-        value_conv = Conv2D(1, kernel_size=1, strides=1, activation='relu', kernel_regularizer=l2(REG_PARAM), padding="same", data_format="channels_first")(tower)
-        value_norm = BatchNormalization(axis=1)(value_conv)  # Batch normalisation
+        value_conv = Conv2D(1, kernel_size=1, strides=1, activation='relu', kernel_regularizer=l2(REG_PARAM), padding="same")(tower)
+        value_norm = BatchNormalization()(value_conv)  # Batch normalisation
         value_rnln1 = ReLU()(value_norm)  # A rectifier non-linearity
         # Make sure the output size is correct
-        value_flat = Flatten(data_format="channels_first")(value_rnln1)
+        value_flat = Flatten()(value_rnln1)
         # A fully connected linear layer to a hidden layer of size 256
         value_dense = Dense(256, kernel_regularizer=l2(REG_PARAM))(value_flat)
         value_rnln2 = ReLU()(value_dense)  # A rectifier non-linearity
@@ -70,7 +70,7 @@ class Network:
         self.model.compile(optimizer='rmsprop', loss=['categorical_crossentropy', 'mean_squared_error'], metrics=['accuracy'])
 
     def evaluate(self, states):
-        input_planes = np.empty((M*T + L, N, N))
+        input_planes = np.empty((N, N, M*T + L))
 
         current_board = states[-1].board
         if states[-1].currentPlayer == 2:
@@ -84,35 +84,35 @@ class Network:
             if len(states) > i:
                 # This plane exists we must fill it
                 # Get the piece planes
-                input_planes[(-L - M*(i + 1)):(-L - M*i)] = get_piece_planes(states[-i].board)
+                input_planes[:, :, (-L - M*(i + 1)):(-L - M*i)] = get_piece_planes(states[-i].board)
 
                 # No repetition planes: not really sure how to create them
                 # Get the repetition
                 # input_planes[(-L - M * i - 2):(-L - M * i)] = get_piece_planes(states[-i])
             else:
                 # Before the game started: empty planes
-                input_planes[(-L - M*(i + 1)):(-L - M*i)] = 0
+                input_planes[:, :, (-L - M*(i + 1)):(-L - M*i)] = 0
 
         # Player 1 = 0, Player 2 = 1
-        input_planes[-7] = np.repeat(states[-1].currentPlayer - 1, N*N).reshape((N, N))  # Player color
-        input_planes[-6] = np.repeat(states[-1].board.fullmove_number, N*N).reshape((N, N))  # Total move count
-        input_planes[-5] = np.repeat(int(bool(castling & chess.BB_A1)), N*N).reshape((N, N))  # Player 1 castling
-        input_planes[-4] = np.repeat(int(bool(castling & chess.BB_A8)), N*N).reshape((N, N))  # Player 8 castling
-        input_planes[-3] = np.repeat(int(bool(castling & chess.BB_H1)), N*N).reshape((N, N))  # Opponent 1 castling
-        input_planes[-2] = np.repeat(int(bool(castling & chess.BB_H8)), N*N).reshape((N, N))  # Opponent 8 castling
-        input_planes[-1] = np.repeat(current_board.halfmove_clock, N*N).reshape((N, N))  # No progress count
+        input_planes[:, :, -7] = np.repeat(states[-1].currentPlayer - 1, N*N).reshape((N, N))  # Player color
+        input_planes[:, :, -6] = np.repeat(states[-1].board.fullmove_number, N*N).reshape((N, N))  # Total move count
+        input_planes[:, :, -5] = np.repeat(int(bool(castling & chess.BB_A1)), N*N).reshape((N, N))  # Player 1 castling
+        input_planes[:, :, -4] = np.repeat(int(bool(castling & chess.BB_A8)), N*N).reshape((N, N))  # Player 8 castling
+        input_planes[:, :, -3] = np.repeat(int(bool(castling & chess.BB_H1)), N*N).reshape((N, N))  # Opponent 1 castling
+        input_planes[:, :, -2] = np.repeat(int(bool(castling & chess.BB_H8)), N*N).reshape((N, N))  # Opponent 8 castling
+        input_planes[:, :, -1] = np.repeat(current_board.halfmove_clock, N*N).reshape((N, N))  # No progress count
         return self.model.predict(np.array([input_planes]), batch_size=1)
 
 
 def get_piece_planes(board):
     fen_order = "KQRBNPkqrbnp"  # Possible optimization: Make this a dictionary
     state = fix_fen(board.fen())
-    result = np.zeros((12, N, N))
+    result = np.zeros((N, N, 12))
     for rank in range(N):
         for file in range(N):
             piece = state[rank * N + file]
             if piece.isalpha():
-                result[fen_order.find(piece)][rank][file] = 1
+                result[rank][file][fen_order.find(piece)] = 1
     return result
 
 
